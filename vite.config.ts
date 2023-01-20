d6c60c74da2b367d60d4a4e2111934938d7a93ea
Mike DeVine <pixelshredder@gmail.com>
2023-01-20 14:46:33 -0800

    checkout vue via vite with ts, added bootstrap-vue

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      vue: '@vue/compat'
+    }
+  },
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 3
+          }
+        }
+      }
+    })
+  ]
+})
